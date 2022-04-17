d3e03f5f2acbac72ee9de04b0155817b2e9132d3
GameGuyOnYT <81768350+GameGuyOnYT@users.noreply.github.com>
2022-04-17 23:16:19 +0300

    Create supportedgames.lua

---
+++ supportedgames.lua
@@ -0,0 +1,15 @@
+local ScreenGui = Instance.new("ScreenGui")
+local TextLabel = Instance.new("TextLabel")
+
+ScreenGui.Parent = game.Workspace
+
+TextLabel.Parent = ScreenGui
+TextLabel.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+TextLabel.Position = UDim2.new(0.111539438, 0, 0.158488393, 0)
+TextLabel.Size = UDim2.new(0, 705, 0, 402)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Normal Scripts\\n- Funky Friday\\n- Pet Simulator X\\n\\nV.G Hub\\n- https://raw.githubusercontent.com/1201for/Funs-and-Features/main/Features-and-games\\n\\nSaza Hub\\n- Blox Fruits, Pet Simulator X, Anime Clicker Simulator, Build A Boat, King Legacy, Two Piece\\n\\nShiny Tool\\n- Pet Simulator X, Clicker Simulator, Magnet Simulator 2, Pet Legacy, Donate Me Pls, Arsenal\\n\\nSoggyware\\n- Anime Dimensions, Rebirth Champions X, Tapper Simulator, Anime Clicker Simulator, Anime Tappers, Slap Battles, Farm Life, Clicking Simulator, Burn Everything Simulator, A One Piece Game, Collect All Pets, Untitled Tag Game, Boom Simulator, Pet Simulator X, Chicken Life, Smashing Simulator X, Slashing Simulator, Timber, Big Man Simulator, Tapping Legends X, Pet Legacy"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 12.000
+TextLabel.TextWrapped = true
