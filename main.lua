9b732a3078b900dea840d24e598b40c1697fc49d
GameGuyOnYT <81768350+GameGuyOnYT@users.noreply.github.com>
2022-04-17 18:51:51 +0300

    Added supported games to hub descriptions
    ewa

--- main.lua
+++ main.lua
@@ -10,22 +10,22 @@ local UniversalTab = Window:NewTab("Settings")
 local UniversalSection4 = UniversalTab:NewSection("Settings")
 local UniversalSection5 = UniversalTab:NewSection("Credits : Efe Tahça")
 
-UniversalSection1:NewButton("Saza Hub", "Script", function() --Creates a button
+UniversalSection1:NewButton("Saza Hub", "Supported Games : Blox Fruits, Pet Simulator X, Anime Clicker Simulator, Build A Boat, King Legacy, Two Piece", function() --Creates a button
     loadstring(game:HttpGet"https://rawscripts.net/raw/SAZA-HUB_496")() --The function called after the button is pressed
 end)
-UniversalSection1:NewButton("V.G Hub", "Script", function() --Creates a button
+UniversalSection1:NewButton("V.G Hub", "Supported Games : https://raw.githubusercontent.com/1201for/Funs-and-Features/main/Features-and-games", function() --Creates a button
     loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))() --The function called after the button is pressed
 end)
-UniversalSection1:NewButton("Shiny Tool", "Script", function() --Creates a button
+UniversalSection1:NewButton("Shiny Tool", "Pet Simulator X, Clicker Simulator, Magnet Simulator 2, Pet Legacy, Donate Me Pls, Arsenal", function() --Creates a button
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/Loader.lua"))() --The function called after the button is pressed
 end)
-UniversalSection3:NewButton("BK Hack", "Script", function() --Creates a button
+UniversalSection3:NewButton("BK Hack", "Pet Simulator X Script", function() --Creates a button
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BLACKGAMER1221/Pet-Simulator-X/main/BK%20Pet.lua"))() --The function called after the button is pressed
 end)
-UniversalSection1:NewButton("Soggyware", "Script", function() --Creates a button
+UniversalSection1:NewButton("Soggyware", "Anime Dimensions, Rebirth Champions X, Tapper Simulator, Anime Clicker Simulator, Anime Tappers, Slap Battles, Farm Life, Clicking Simulator, Burn Everything Simulator, A One Piece Game, Collect All Pets, Untitled Tag Game, Boom Simulator, Pet Simulator X, Chicken Life, Smashing Simulator X, Slashing Simulator, Timber, Big Man Simulator, Tapping Legends X, Pet Legacy", function() --Creates a button
     loadstring(game:HttpGet('https://raw.githubusercontent.com/SunkenMuch/Soggyware/main/Main',true))() --The function called after the button is pressed
 end)
-UniversalSection2:NewButton("Funky Friday Autoplayer", "Script", function() --Creates a button
+UniversalSection2:NewButton("Funky Friday Autoplayer", "Funky Friday Script", function() --Creates a button
     loadstring(game:HttpGet"https://rawscripts.net/raw/SAZA-HUB_496")() --The function called after the button is pressed
 end)
 
