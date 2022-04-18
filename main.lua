02d5dd499a7ba45b88ff24723f44fd6aecac1353
GameGuyOnYT <81768350+GameGuyOnYT@users.noreply.github.com>
2022-04-18 20:32:57 +0300

    ITTTT WORKSSSSSSSSSSSSSSSSSS ...?
    dfseadfasdasdwas

--- main.lua
+++ main.lua
@@ -1,38 +1,33 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() --Gets GUI lib
-local Window = Library.CreateLib("GameGuy's Script Hub", "Ocean") -- Creates the window
-
-local UniversalTab = Window:NewTab("Scripts") --Creates new tab
-local UniversalSection1 = UniversalTab:NewSection("General") -- Creates a section in the tab
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("GameGuy's Script Hub", "Ocean")
+local UniversalTab = Window:NewTab("Scripts")
+local UniversalSection1 = UniversalTab:NewSection("General")
 local UniversalSection2 = UniversalTab:NewSection("Funky Friday")
 local UniversalSection3 = UniversalTab:NewSection("Pet Simulator X")
-
 local UniversalTab = Window:NewTab("Settings")
 local UniversalSection4 = UniversalTab:NewSection("Settings")
 local UniversalSection5 = UniversalTab:NewSection("Credits : Efe Tahça")
-
-UniversalSection1:NewButton("Saza Hub", "Executes Saza Hub", function() --Creates a button
-    loadstring(game:HttpGet"https://rawscripts.net/raw/SAZA-HUB_496")() --The function called after the button is pressed
+UniversalSection1:NewButton("Saza Hub", "Executes Saza Hub", function()
+    loadstring(game:HttpGet"https://rawscripts.net/raw/SAZA-HUB_496")()
 end)
-UniversalSection1:NewButton("V.G Hub", "Executes V.G Hub", function() --Creates a button
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))() --The function called after the button is pressed
+UniversalSection1:NewButton("V.G Hub", "Executes V.G Hub", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub"))()
 end)
 UniversalSection1:NewButton("Shiny Tool", "Executes ShinyTool", function() --Creates a button
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/Loader.lua"))() --The function called after the button is pressed
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/ShinyToolV2/main/Loader.lua"))()
 end)
 UniversalSection3:NewButton("BK Hack", " Executes BK Hack", function() --Creates a button
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BLACKGAMER1221/Pet-Simulator-X/main/BK%20Pet.lua"))() --The function called after the button is pressed
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BLACKGAMER1221/Pet-Simulator-X/main/BK%20Pet.lua"))()
 end)
 UniversalSection1:NewButton("Soggyware", "Executes Soggyware", function() --Creates a button
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/SunkenMuch/Soggyware/main/Main',true))() --The function called after the button is pressed
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SunkenMuch/Soggyware/main/Main"))()
 end)
 UniversalSection2:NewButton("Funky Friday Autoplayer", "Funky Friday Script", function() --Creates a button
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/funky-friday-autoplay/main/main.lua"))() --The function called after the button is pressed
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/funky-friday-autoplay/main/main.lua"))()
 end)
 UniversalSection1:NewButton("Supported Games", "Shows Supported Games.", function() --Creates a button
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/GameGuyOnYT/GameGuyScriptHub/main/supportedgames.lua"))() --The function called after the button is pressed
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GameGuyOnYT/GameGuyScriptHub/main/supportedgames.lua"))() 
 end)
-
-UniversalSection4:NewKeybind("Hide (Default P)", "Hide", Enum.KeyCode.P, function() --Creates a keybind to...
-	Library:ToggleUI() --...hide the GUI.
+UniversalSection4:NewKeybind("Hide (Default P)", "Hide", Enum.KeyCode.P, function()
+	Library:ToggleUI()
 end)
-
