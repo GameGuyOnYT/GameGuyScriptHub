3509befe950a1b8a6161fcd478bcddbc20f36c58
GameGuyOnYT <81768350+GameGuyOnYT@users.noreply.github.com>
2022-04-18 19:33:59 +0300

    dasdas
    sdadadas

---
+++ supportedgames.lua
@@ -0,0 +1,26 @@
+local ScreenGui = Instance.new("ScreenGui")
+local main = Instance.new("Frame")
+local label = Instance.new("TextLabel")
+
+ScreenGui.Parent = game.CoreGui
+
+main.Name = "main"
+main.Parent = ScreenGui
+main.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+main.BorderColor3 = Color3.fromRGB(255, 255, 255)
+main.Position = UDim2.new(0.171806172, 0, 0.18274112, 0)
+main.Size = UDim2.new(0, 653, 0, 384)
+main.Draggable = true
+
+label.Name = "label"
+label.Parent = main
+label.BackgroundColor3 = Color3.fromRGB(86, 76, 251)
+label.BorderColor3 = Color3.fromRGB(255, 255, 255)
+label.Position = UDim2.new(0.0194999855, 0, 0.0349777527, 0)
+label.Size = UDim2.new(0, 630, 0, 360)
+label.Font = Enum.Font.Highway
+label.Text = "Normal Scripts\\n- Funky Friday\\n- Pet Simulator X\\n\\nV.G Hub\\n- https://raw.githubusercontent.com/1201for/Funs-and-Features/main/Features-and-games\\n\\nSaza Hub\\n- Blox Fruits, Pet Simulator X, Anime Clicker Simulator, Build A Boat, King Legacy, Two Piece\\n\\nShiny Tool\\n- Pet Simulator X, Clicker Simulator, Magnet Simulator 2, Pet Legacy, Donate Me Pls, Arsenal\\n\\nSoggyware\\n- Anime Dimensions, Rebirth Champions X, Tapper Simulator, Anime Clicker Simulator, Anime Tappers, Slap Battles, Farm Life, Clicking Simulator, Burn Everything Simulator, A One Piece Game, Collect All Pets, Untitled Tag Game, Boom Simulator, Pet Simulator X, Chicken Life, Smashing Simulator X, Slashing Simulator, Timber, Big Man Simulator, Tapping Legends X, Pet Legacy"
+label.TextColor3 = Color3.fromRGB(255, 255, 255)
+label.TextScaled = true
+label.TextSize = 14.000
+label.TextWrapped = true
