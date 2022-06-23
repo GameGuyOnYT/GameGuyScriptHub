d0056e9e766074920284c2a21964e9c683136807
GameGuy <efeoyun@tahca.com>
2022-06-23 16:29:17 +0300

    Update openAutoRebirth.lua

--- ClickerMadness/openAutoRebirth.lua
+++ ClickerMadness/openAutoRebirth.lua
@@ -3,7 +3,7 @@ local pathMadness = game:GetService("ReplicatedStorage").Aero.AeroRemoteServices
 spawn(function()
     while wait() do
         if not _G.autoRebirth then break end
-        pathMadness.RebirthService.BuyRebirths:FireServer(1)
+        pathMadness.RebirthService.BuyRebirths:FireServer(1000000)
     end
 end)
 print('Rebirthing!')
